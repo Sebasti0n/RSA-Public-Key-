f4c5545b874e8cd0c2897587fe1b371b7a09a24d
Sebastian <38714137+Sebasti0n@users.noreply.github.com>
2022-04-28 16:31:03 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,7 @@ RSA is a asymetric encryption algoritm
 
 #include <iostream>
 #include <math.h>
+#include <cmath>
 
 
 using namespace std;
@@ -21,13 +22,25 @@ int gcd(int a, int h) //Greatest Common Divisor for Totient Function
 int main()
 {
 	//Get input for two prime numbers p and q (range 5000 to 10000)
-	int p; 
-	int q; 
+	double p = 5000; 
+	double q = 10000; 
+	double n = p * q; 
+	double count; 
+	double totient = (p - 1) * (q - 1); //totient function
 
-	//verify that meets the range 
-	for (i, i > 0, i++)
+
+
+//Generate public key: e based on n and the totient value 0
+	double e = 2; 
+
+//check co-prime satisfies e > 1
+	while (e < totient)
 	{
-
+		count = gcd(e, totient); 
+		if (count == 1)
+			break;
+		else
+			e++; 
 	}
 
 	//Compute and display modulo n 
@@ -36,11 +49,20 @@ int main()
 
 
 	//Generate private key: d 
-
-	//Generate public key: e based on n and the totient value 0
+	double d; 
+	double k = 2; 
+	d = (1 + (k * totient)) / e; 
+	double msg = 12; 
+	double c = pow(msg, e); 
+	double m = pow(c, d); 
+	c = modf(c, n); 
+	m = modf(m, n); 
 
 	//Verify that d and e are multiclative inverse of each other based on 0
 
+	cout << "Message data = " << msg; 
+	cout << "\n" << "p = " << p; 
+	cout << 
 	system ("pause");
 	return 0; 
 }
